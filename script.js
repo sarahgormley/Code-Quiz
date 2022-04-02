0dfa2368ad1ccee5080cb150d00b45ae092a176a
SarahGormley <sarah.gormley39@gmail.com>
2022-04-02 11:53:15 +1000

    Initial commit

---
+++ script.js
@@ -0,0 +1,68 @@
+// Variables
+//Var for timer class 
+
+//Timer
+
+// Questions
+
+var questions = [{
+    question: "What does HTML stand for?",
+    choices: ["Hot Mail", "How to Make Lasagna", "Hot Take Markup Language", "Hyper Text Markup Language"],
+    correctAnswer: 3
+}, {
+    question: "</br> What type of tag is this?",
+    choices: ["Break tag", "Broken tag", "An opening tag"],
+    correctAnswer: 0
+}, {
+    question: "</body> Is this an opening tag or a closing tag?",
+    choices: ["Opening", "Closing", "Neither"],
+    correctAnswer: 1
+}, {
+    question: "Which of the below is the abbreviation of CSS?",
+    choices: ["Cascade Style Sheets", "Color and Style Sheets", "Cascading Style Sheets", "Coded Style Sheet"],
+    correctAnswer: 2
+}, {
+    question: "Which of the below CSS properties is used to change the background color of CSS ?",
+    choices: ["bg color", "color-background", "background-color", "color"],
+    correctAnswer: 2
+}, {
+    question: "Which element is used to represent the transparency of an element in CSS ?",
+    choices: ["Hover", "Opacity", "Transparent", "Overlay"],
+    correctAnswer: 1
+}, {
+    question: "Inside which HTML element do we put the JavaScript?",
+    choices: ["<script>", "<js>", "<scripting>", "<javascript>"],
+    correctAnswer: 0
+}, {
+    question: "How do you write 'Hello World' in an alert box?",
+    choices: ['alertBox("Hello World");', 'msg("Hello World");', 'msgBox("Hello World");', 'alert("Hello World");'],
+    correctAnswer: 3
+}, {
+    question: "How do you write an IF statement in JavaScript?",
+    choices: ["if (i == 5)", "if i = 5 then", "if i == 5 then", "if i = 5"],
+    correctAnswer: 0
+}, {
+    question: "How can you add a comment in a JavaScript?",
+    choices: ["//This is a comment", "'This is a comment'", "<!--This is a comment-->"],
+    correctAnswer: 0
+}, {
+    question: "What is the correct way to write a JavaScript array?",
+    choices: ['var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")', 'var colors = (1:"red", 2:"green", 3:"blue")', 'var colors = "red", "green", "blue"', 'var colors = ["red", "green", "blue"]'],
+    correctAnswer: 3
+}, {
+    question: "Which event occurs when the user clicks on an HTML element?",
+    choices: ["onchange", "onmouseclick", "onclick", "onmouseover"],
+    correctAnswer: 2
+}, {
+    question: "Which operator is used to assign a value to a variable?",
+    choices: ["x", "=", "*", "-"],
+    correctAnswer: 1
+}, {
+    question: "What is the correct HTML for inserting an image?",
+    choices: ['<image src="image.gif" alt="MyImage">', '<img src="image.gif" alt="MyImage">', '<img alt="MyImage">image.gif</img>', '<img href="image.gif" alt="MyImage">'],
+    correctAnswer: 1
+}, {
+    question: "Which HTML element is used to specify a footer for a document or section?",
+    choices: ["<bottom>", "<section>", "<footer>", "<aside>"],
+    correctAnswer: 2
+}];
