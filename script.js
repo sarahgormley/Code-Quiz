2297d992604d1f4f7400c9b723a9ede751e429a8
SarahGormley <sarah.gormley39@gmail.com>
2022-04-06 23:34:18 +1000

    Finalised styling. Commented CSS and Javascript

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Questions
+// Quiz questions
 var questions = [{
     question: "What does HTML stand for?",
     choices: ["Hot Mail", "How to Make Lasagna", "Hot Take Markup Language", "Hyper Text Markup Language"],
@@ -60,20 +60,25 @@ var questions = [{
     choices: ["<bottom>", "<section>", "<footer>", "<aside>"],
     correctAnswer: "<footer>"
 }];
-// Variables
 
+// Variables
+//Time variables
 var timer = document.getElementById("timer");
 var timeLeft = document.getElementById("time-left");
-var timesUp = document.getElementById("times-up")
+var timesUp = document.getElementById("times-up");
+var secsLeft = 120;
+var timerInterval;
 
+//Start Container variable
 var startContainer = document.getElementById("start-container");
 
+//Button variables
 var startBtn = document.getElementById("start-btn");
 var submitBtn = document.getElementById("submit-btn");
 var restartBtn = document.getElementById("restart-btn");
 var backBtn = document.getElementById("back-btn")
-var clearBtn = document.getElementById("clear-btn")
 
+//Variables for Quiz container and it's contents (Question title, question check, answer check, and buttons)
 var quizContainer = document.getElementById("quiz-container");
 var questionTitle = document.getElementById("question-title");
 var answerCheck = document.getElementById("answer-check");
@@ -82,25 +87,28 @@ var answer2 = document.getElementById("btn2")
 var answer3 = document.getElementById("btn3")
 var answer4 = document.getElementById("btn4")
 
+//Variables for Summary container and it's contents (final score & initials form)
 var summary = document.getElementById("summary");
 var finalScore = document.getElementById("final-score");
 var initials = document.getElementById("initials");
 
+//Variables for high scores continer and it's contents (List of scores and view high score link)
 var highscores = document.getElementById("highscores");
 var viewHighScore = document.getElementById("view-highscore");
 var scoreList = document.getElementById("list-of-scores");
 
+// Variables for quiz values
 var correctAns = 0;
 var questionNum = 0;
 var scoreResult = 0;
 var questionInd = 0;
 var questionRandom;
 
-var secsLeft = 120;
-var timerInterval;
 
+// Quiz loads and runs startScreen function
 startScreen();
 
+// Function for starting screen
 function startScreen() {
     startContainer.style.display = "block"
     timesUp.style.display = "none"
@@ -109,7 +117,7 @@ function startScreen() {
     quizContainer.style.display = "none"
 }
 
-//When start button is clicked
+//When start button is clicked, newQuiz function runs, which starts a new quiz (displays the quiz content and starts the timer)
 function newQuiz() {
     questionInd = 0;
     secsLeft = 120;
@@ -123,7 +131,7 @@ function newQuiz() {
     timer.style.display = "block"
     timesUp.style.display = "none"
 
-    //Starts timer
+    //Starts timer, runs gameOver function once timer reaches 0
     timerInterval = setInterval(function() {
         secsLeft--;
         timeLeft.textContent = secsLeft + " seconds left";
@@ -144,14 +152,14 @@ function newQuiz() {
 
 }
 
-//function showQuiz 
+//Function showQuiz cycles through questions. 
 function displayQuiz() {
     cycleQuestions();
 }
-// show next question
+//cycleQuestions functions puts the question variables into the quiz
 function cycleQuestions() {
 
-    //questionRandom = questions[Math.floor(Math.random() * questions.length)]
+    //Assigns the question choices to the corresponding button
 
     questionTitle.textContent = questions[questionInd].question;
     answer1.textContent = questions[questionInd].choices[0];
@@ -161,23 +169,21 @@ function cycleQuestions() {
 }
 
 
-//create function to check answer
-//if else rules for checking answer
-//rule for continuing thru questions if/else
+//Function to check answer selected. If/else statements for if the user choice is correct or incorrect
+
 function checkAns(correctAnswer) {
     if (questions[questionInd].correctAnswer === questions[questionInd].choices[correctAnswer]) {
         scoreResult++;
         answerCheck.textContent = "Correct!"
         answerCheck.style.color = "green";
 
-        //console.log("it worked!")
-        //console.log(scoreResult)
+
     } else {
         secsLeft -= 10;
         answerCheck.textContent = "Incorrect!";
         answerCheck.style.color = "red";
 
-        //console.log("wrong answer")
+
     }
 
     questionInd++;
@@ -195,9 +201,8 @@ function checkAns(correctAnswer) {
         gameOver();
     }
 }
-//console.log(questions[0].choices[1])
 
-//functions x4 for answer chosen
+//functions x4 for checking the button clicked against which answer it corresponds to
 function choose0() { checkAns(0) };
 
 function choose1() { checkAns(1) };
@@ -206,8 +211,9 @@ function choose2() { checkAns(2) };
 
 function choose3() { checkAns(3) };
 
+//Function for end of game. Shows summary and score. 
 function gameOver() {
-    //console.log("game over!")
+
     startContainer.style.display = "none";
     summary.style.display = "block";
     highscores.style.display = "none";
@@ -220,14 +226,9 @@ function gameOver() {
 
 
 
-//function for st0ring high scores locally
+//function for storing high scores locally
 function storeHighScores(event) {
     event.preventDefault();
-
-    if (initials.value === "") {
-        alert("Please enter initials!");
-        return;
-    }
 
     var storedHighScore = localStorage.getItem("high scores");
     var arrayScores;
@@ -253,7 +254,7 @@ function storeHighScores(event) {
 
     showScores();
 }
-
+//Function for pulling high scores from local storage and showing them. 
 var i = 0
 
 function showScores() {
@@ -283,33 +284,34 @@ function showScores() {
 
 
 
-//event listners for buttons
+//Event listners for buttons
 startBtn.addEventListener("click", newQuiz);
 answer1.addEventListener("click", choose0);
 answer2.addEventListener("click", choose1);
 answer3.addEventListener("click", choose2);
 answer4.addEventListener("click", choose3);
 
+//Event listener to restart quiz
 restartBtn.addEventListener("click", function() {
     startContainer.style.display = "block";
     highscores.style.display = "none";
     summary.style.display = "none"
 
-})
+});
 
+//Event listener for submitting scores to local storage
 submitBtn.addEventListener("click", function(event) {
     storeHighScores(event);
 });
 
+//Event listener for viewing high scores
 viewHighScore.addEventListener("click", function(event) {
     showScores(event);
 })
 
+//Event listener for clicking back button
 backBtn.addEventListener("click", function() {
     startContainer.style.display = "block";
     highscores.style.display = "none"
 
 })
-clearBtn.addEventListener("click", function() {
-    localStorage.removeItem("high scores");
-});
